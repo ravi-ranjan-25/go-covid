2b528b1db43c89db0db33150001a993652073813
Your Name <raviranjanv25@gmail.com>
2020-05-11 15:35:44 +0530

    gg

--- covid/settings.py
+++ covid/settings.py
@@ -40,10 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'covidscrap',
-    
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -83,6 +84,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Password validation
